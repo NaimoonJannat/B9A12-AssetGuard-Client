8866d60cdcd9ee6c12115e636d61f2af7d7682a6
NaimoonJannat <naimoonjannat@gmail.com>
2024-06-02 21:42:25 +0600

    Firebase setup done

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,15 @@ import {
   RouterProvider, 
  } from "react-router-dom"; 
 import router from './Routes/Routes.jsx';
+import AuthProvider from './providers/AuthProvider.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} /> 
+    <ToastContainer />
+    </AuthProvider>
   </React.StrictMode>,
 )
