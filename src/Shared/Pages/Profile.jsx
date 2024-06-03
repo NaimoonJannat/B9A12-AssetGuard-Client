3a8991584870eeddee6e0370524f09c8f835407b
NaimoonJannat <naimoonjannat@gmail.com>
2024-06-04 00:26:21 +0600

    Update profile

--- src/Shared/Pages/Profile.jsx
+++ src/Shared/Pages/Profile.jsx
@@ -1,7 +1,100 @@
+import { useContext, useState } from "react";
+import { updateProfile } from "firebase/auth";
+import { AuthContext } from "../../providers/AuthProvider";
+import Title from "../Title";
+import useFetchUserData from "../../hooks/useFetchUserData";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Swal from "sweetalert2";
+
 const Profile = () => {
+    const { user, updateUserProfile } = useContext(AuthContext);
+    const { userData } = useFetchUserData(user?.email);
+
+    const [displayName, setDisplayName] = useState(user?.displayName || "");
+    const [photoURL, setPhotoURL] = useState(user?.photoURL || "");
+    const [showModal, setShowModal] = useState(false);
+    const formattedBirthdate = userData?.birthdate ? new Date(userData.birthdate).toISOString().split('T')[0] : '';
+
+    const handleUpdateButton = () => {
+        setShowModal(true);
+    };
+
+    const handleUpdateProfile = () => {
+        updateProfile(user, {
+            displayName,
+            photoURL
+        }).then(() => {
+            updateUserProfile(displayName, photoURL);
+            Swal.fire("Success", "Profile updated successfully!", "success");
+        }).catch(error => {
+            console.error("Error updating profile: ", error);
+            Swal.fire("Error", "Error updating profile. Please try again.", "error");
+        }).finally(() => {
+            setShowModal(false);
+        });
+    };
+
     return (
-        <div>
-            <h2>profile</h2>
+        <div className="py-20">
+            <Title title="My Profile" subtitle="Profile Information"></Title>
+            <div className="flex flex-col justify-center m-8 text-center">
+                <img
+                    alt=""
+                    className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full bg-gray-500"
+                    src={user?.photoURL}
+                />
+                <p className="text-xl font-semibold leading-tight">{user?.displayName}</p>
+                <p className="text-gray-400">Role: {userData?.role}</p>
+                <p className="text-gray-400">Date of Birth: {formattedBirthdate}</p>
+                <p className="text-gray-400">Email: {userData?.email}</p>
+                <button
+                    onClick={handleUpdateButton}
+                    className="btn w-20 mx-auto bg-[#57A6A1] text-white"
+                >
+                    Update
+                </button>
+            </div>
+            {showModal && (
+                <div className="fixed inset-0 z-10 flex items-center justify-center">
+                    <div className="absolute inset-0 bg-black opacity-50"></div>
+                    <div className="relative bg-white rounded-lg p-8 max-w-md">
+                        <span className="absolute top-0 right-0 p-2 cursor-pointer" onClick={() => setShowModal(false)}>
+                            &times;
+                        </span>
+                        <form className="space-y-4" onSubmit={handleUpdateProfile}>
+                            <label className="input input-bordered flex items-center gap-2">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    defaultValue={user?.displayName}
+                                    onChange={(e) => setDisplayName(e.target.value)}
+                                    className="grow text-gray-500"
+                                    placeholder="Your Name"
+                                    required
+                                />
+                            </label>
+                            <label className="input input-bordered flex items-center gap-2">
+                                <input
+                                    type="text"
+                                    name="url"
+                                    defaultValue={user?.photoURL}
+                                    onChange={(e) => setPhotoURL(e.target.value)}
+                                    className="grow text-gray-500"
+                                    placeholder="Photo URL"
+                                    required
+                                />
+                            </label>
+                            <button
+                                type="submit"
+                                className="btn w-full bg-[#57A6A1] text-white"
+                            >
+                                Update Profile
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -4,12 +4,9 @@ import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
-
-
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
-
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
@@ -23,54 +20,79 @@ const AuthProvider = ({children}) => {
         return createUserWithEmailAndPassword(auth, email, password)
             .then(userCredential => {
                 const user = userCredential.user;
-    
                 // Update the profile with the provided name and photoURL
                 return updateProfile(user, {
                     displayName: name,
                     photoURL: photo
-                }).then(() => userCredential); // Return userCredential after profile update
+                }).then(() => {
+                    setUser(user);
+                    return userCredential; // Return userCredential after profile update
+                });
             })
             .catch(error => {
+                setLoading(false);
                 throw error;
             });
     }
-    
-    
-    const signInGoogle = () =>{
+
+    const signInGoogle = () => {
         setLoading(true);
-        return signInWithPopup(auth, provider);
+        return signInWithPopup(auth, provider).then((result) => {
+            setUser(result.user);
+        }).finally(() => {
+            setLoading(false);
+        });
     }
 
-    const signIn = (email, password) =>{
+    const signIn = (email, password) => {
         setLoading(true);
-        return signInWithEmailAndPassword(auth, email, password);
+        return signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            setUser(userCredential.user);
+        }).finally(() => {
+            setLoading(false);
+        });
     }
 
     const logOut = () => {
         setLoading(true);
-        return signOut(auth);
+        return signOut(auth).then(() => {
+            setUser(null);
+        }).finally(() => {
+            setLoading(false);
+        });
     }
 
-    useEffect(()=>{
-        const unSubscribe =
-        onAuthStateChanged(auth, currentUser=>{
+    const updateUserProfile = (displayName, photoURL) => {
+        if (user) {
+            return updateProfile(user, { displayName, photoURL }).then(() => {
+                setUser({ ...user, displayName, photoURL });
+            });
+        }
+        return Promise.reject("No user is currently signed in.");
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             setLoading(false);
         });
         return () => {
             unSubscribe();
         }
-    },[])
+    }, []);
+
     const isLoggedIn = !!user;
 
-    const authInfo ={
-        user, 
+    const authInfo = {
+        user,
         loading,
         createUser,
         logOut,
         signIn,
         signInGoogle,
-        isLoggedIn
+        updateUserProfile,
+        isLoggedIn,
+        setUser // Exposing setUser for direct updates in Profile component
     }
 
     return (
