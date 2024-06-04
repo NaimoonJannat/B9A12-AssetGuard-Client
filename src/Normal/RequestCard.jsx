12519e327d2d8ff08810dd00a13068f4ea8d86fa
NaimoonJannat <naimoonjannat@gmail.com>
2024-06-04 22:53:19 +0600

    Request cards shown

--- src/Normal/RequestCard.jsx
+++ src/Normal/RequestCard.jsx
@@ -1,10 +1,39 @@
 
 
-const RequestCard = () => {
+const RequestCard = ({request}) => {
+    const { product, type, requestDate, approveDate, status} = request;
     return (
-        <div>
-            
+        <div className="text-left border-2 border-[#240570] p-6 rounded-lg">
+	<div className="flex justify-between pb-4 border-bottom">
+		<div className="flex flex-col">
+			<div className="font-semibold">Request Date: {requestDate}</div>
+			<div className="font-semibold">Approve Date: {approveDate}</div>
+		</div>
+	</div>
+	<div className="space-y-4">
+		<div className="space-y-2">
+				<h3 className="text-xl font-semibold">{product}</h3>
+            <div className="flex gap-1">
+                <div className="btn btn-xs border-1 bg-[#57A6A1] text-white border-[#240570]">
+                {type}
+                </div>
+                <div className="btn btn-xs border-1 bg-[#240570] text-white border-[#57A6A1]">
+                {status}
+                </div>
+            </div>
         </div>
+            <div className="flex gap-2 justify-center items-center">
+{/*             
+            <button onClick={handleApproveButton} className="btn btn-circle text-[#3bbd3bb0] border-1 border-[#240570]"><FaCheck /></button>
+            
+            <button onClick={() => handleDelete(_id)} className="btn btn-circle text-[#F50000] border-1 border-[#240570]">
+            <RxCross2 />
+            </button > */}
+        </div>
+            
+	</div>
+
+</div>
     );
 };
 
--- src/Normal/RequestedAssets.jsx
+++ src/Normal/RequestedAssets.jsx
@@ -1,10 +1,26 @@
+import { useLoaderData } from "react-router-dom";
 import Title from "../Shared/Title";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import RequestCard from "./RequestCard";
 
 const RequestedAssets = () => {
+    const myRequests = useLoaderData();
+    const {user} = useContext(AuthContext);
+    const myEmail = user.email;
+    const filteredData =myRequests.filter(user => user.useremail === myEmail);
     return (
-        <div>
+        <div className="py-20">
             <div>
             <Title title="My Requested Assets" subtitle="All Requested Assets"></Title>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {filteredData.map(request => (
+                    <RequestCard
+                        key={request._id}
+                        request={request}
+                    />
+                ))}
+            </div>
         </div>
         </div>
     );
