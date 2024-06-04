7a07e692224affe9a5d10275cc292b5854b2a624
NaimoonJannat <naimoonjannat@gmail.com>
2024-06-05 00:43:17 +0600

    Limited stock done

--- src/Hr/HomeHr/HomeHr.jsx
+++ src/Hr/HomeHr/HomeHr.jsx
@@ -1,15 +1,17 @@
 import HomeHrAssets from './HomeHrAssets';
 import HomeHrBanner from './HomeHrBanner';
 import HomePending from './HomePending';
+import LimitedStock from './LimitedStock';
 
 const HomeHr = () => {
   
     return (
         <div>
             <HomeHrBanner></HomeHrBanner>
-            <div className='w-full md:w-4/5 mx-auto'>
+            <div className='w-full md:w-4/5 mx-auto space-y-20'>
             <HomePending></HomePending>
             <HomeHrAssets></HomeHrAssets>
+            <LimitedStock></LimitedStock>
             </div>
         </div>
     );
---
+++ src/Hr/HomeHr/LimitedStock.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import Title from "../../Shared/Title";
+import AssetCard from "../AssetCard";
+
+const LimitedStock = () => {
+    const [requests, setRequests] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        // Function to fetch data
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/assets');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setRequests(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+    const filteredAssets = requests.filter(asset => asset.quantity <= 9 && asset.quantity > 0 );
+    return (
+        <div>
+        <div>
+            <Title title="Limited Stock Assets" subtitle="Assets Almost Out of Stock"></Title>
+        </div>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error}</p>}
+        {!loading && !error && (
+             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+             {filteredAssets.map(request => (
+                 <AssetCard
+                 key={request._id}
+                 asset={request}
+                 ></AssetCard>
+             ))}
+         </div>
+        )}
+    </div>
+    );
+};
+
+export default LimitedStock;
